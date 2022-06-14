38b44b7d6116e94213f0b4e143fb576f1256f1b8
unknown <bgw595@myy.haaga-helia.fi>
2022-06-14 22:58:41 +0300

    fourth com

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sdsdddsdsddsdssdsdI am the ID fan.
+          I am the ID fan.
         </p>
         <a
           className="App-link"
