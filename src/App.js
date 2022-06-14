23952a935948e4f69120e5effffa3008a420d184
unknown <bgw595@myy.haaga-helia.fi>
2022-06-14 22:44:24 +0300

    init com

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          sdsdddsdsddsdssdsdI am the ID fan.
         </p>
         <a
           className="App-link"
